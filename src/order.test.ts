fb329575d0ada3f91b0843b64185114c8e42b982
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-01-13 18:14:33 +0100

    Should calculate order sub total

--- src/order.test.ts
+++ src/order.test.ts
@@ -1,12 +1,12 @@
 import Item from "./Item";
 import Order from "./Order";
 
-test('Order Test', function () {
+test('Should calculate order sub total', function () {
     const order = new Order();
-    order.addItem(new Item('Cigarro', 'Marlbor', 10));
+    order.addItem(new Item('Cigar', 'Marlbor', 10));
     order.addItem(new Item('Beer', 'Itaipava', 5));
     order.addItem(new Item('Water', 'Preciosa', 2));
- const subtotal=order.getSubtotal()
+    const subtotal=order.getSubtotal()
 
     expect(subtotal).toBe(17)
 
